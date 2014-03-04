05fa58a0f24415aa733944f1aa9a638cb0cb53f3
Pontus Freyhult <pontus_bils_account_registry@soua.net>
2014-03-04 08:46:56 +0100

    Use Swamid IdPs only

--- mdupdate.sh
+++ mdupdate.sh
@@ -1,1 +1,4 @@
-wget -q -O idp.xml https://md.swamid.se/md/swamid-idp-transitive.xml
+wget -q -O idp.xml https://md.swamid.se/md/swamid-idp.xml
+
+
+#https://md.swamid.se/md/swamid-idp-transitive.xml
